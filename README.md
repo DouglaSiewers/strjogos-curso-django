03f6fea1b1e3ba9484838ba5fc819bb0c2515c6a
DouglaSiewers <douglassiewers@gmail.com>
2021-11-17 10:45:19 -0300

    Autenticação e autorização dos endpoints

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # strjogos-curso-django
      Esse sistema se trata de uma plataforma de streaming de jogos, onde os usuários podem escolher um pacote de assinatura contendo jogos determinados e joga-los via streaming.
+
+     Permissões : 
+     Grupo Administradores: Tem todas as permissões
+     Grupo Assinantes: Pode visualizar generos, jogos, motro grafico, pacotes de assinatura e desenvolvedora. Podendo adicionar e visualizar assinatura, e adicionar,  mudar, deletar e visualizar itens da assinatura
--- strjogos/urls.py
+++ strjogos/urls.py
@@ -1,28 +1,34 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from rest_framework import routers
 
 from core import views
 
 router = routers.DefaultRouter()
-router.register(r'generos', views.GeneroViewSet)
-router.register(r'motorgrafico', views.MotorGraficoViewSet)
-router.register(r'desenvolvedoras', views.DesenvolvedoraViewSet)
-router.register(r'jogos', views.JogoViewSet)
-router.register(r'pacotesAssinatura', views.PacoteAssinaturaViewSet)
-router.register(r'Assinatura', views.AssinaturaViewSet)
+router.register(r"generos", views.GeneroViewSet)
+router.register(r"motorgrafico", views.MotorGraficoViewSet)
+router.register(r"desenvolvedoras", views.DesenvolvedoraViewSet)
+router.register(r"jogos", views.JogoViewSet)
+router.register(r"pacotesAssinatura", views.PacoteAssinaturaViewSet)
+router.register(r"Assinatura", views.AssinaturaViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('teste/', views.teste),
-    path('pag2/', views.teste2),
-    path('generos-class/', views.GeneroView.as_view()),
-    path('generos-class/<int:id>/', views.GeneroView.as_view()),
-    path('generos-apiview/', views.GeneroList.as_view()),
-    path('generos-apiview/<int:id>/', views.GeneroDetail.as_view()),
-    path('generos-generic/', views.GeneroListGeneric.as_view()),
-    path('generos-generic/<int:id>/', views.GeneroDetailGeneric.as_view()),
-    path('', include(router.urls))
-
+    path("admin/", admin.site.urls),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("teste/", views.teste),
+    path("pag2/", views.teste2),
+    path("generos-class/", views.GeneroView.as_view()),
+    path("generos-class/<int:id>/", views.GeneroView.as_view()),
+    path("generos-apiview/", views.GeneroList.as_view()),
+    path("generos-apiview/<int:id>/", views.GeneroDetail.as_view()),
+    path("generos-generic/", views.GeneroListGeneric.as_view()),
+    path("generos-generic/<int:id>/", views.GeneroDetailGeneric.as_view()),
+    path("", include(router.urls)),
 ]
